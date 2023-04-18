c2fb4bf2d552d78a758edab63bf92e3364b3ca10
jjibrah <ibrabdi109@gmail.com>
2023-04-18 08:30:17 +0300

    first commit

---
+++ app/src/main/java/com/muthoknee/morningfirebasedbapp/User.kt
@@ -0,0 +1,21 @@
+package com.muthoknee.morningfirebasedbapp
+
+class User {
+    var name:String = ""
+    var email:String = ""
+    var idNumber:String = ""
+    var id:String = ""
+    var itemSelected:String = ""
+
+
+
+    constructor()
+    constructor(name: String, email: String, idNumber: String, id: String, itemSelected: String) {
+        this.name = name
+        this.email = email
+        this.idNumber = idNumber
+        this.id = id
+        this.itemSelected = itemSelected
+    }
+
+}
